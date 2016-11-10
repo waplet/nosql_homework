9ecc6a4198b417cf72d6b055c7c755a90f93901f
Maris Jankovskis <maarisjankovskis@gmail.com>
2016-11-10 22:41:35 +0200

    asd

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,7 +6,7 @@
         <div class="col-md-12">
             <div class="panel panel-default">
                 <div class="panel-heading">Dashboard</div>
-                <table class="table table-condensed" style="table-layout: fixed;width:100%;">
+                <table class="table table-condensed log-table" style="table-layout: fixed;width:100%;">
                     <thead>
                         <tr>
                             <th>ID</th>
@@ -21,14 +21,14 @@
                 @if ($logs)
                     @foreach ($logs as $log)
                         <tr class="{{ \App\Helper::getSeverityClass($log->severity) }}">
-                            <td style="width:5%;overflow:hidden;white-space: nowrap; text-overflow: ellipsis;"><a href="{{ url('/log/' . $log->id) }}" title="{{ $log->id }}">Open</a></td>
-                            <td style="width:10%;overflow:hidden;white-space: nowrap; text-overflow: ellipsis;">{{ \App\Helper::dateToDiff($log->creation_date) }}<br/>
+                            <td class="item"><a href="{{ url('/log/' . $log->id) }}" title="{{ $log->id }}">Open</a></td>
+                            <td class="item" style="width:10%;">{{ \App\Helper::dateToDiff($log->creation_date) }}<br/>
                                 <span class="small">{{ $log->creation_date }}</span></td>
-                            <td style="width:10%;overflow:hidden;white-space: nowrap; text-overflow: ellipsis;">{{ \App\Models\Project::getProjectName($log->project_id) }}</td>
-                            <td style="width:10%;overflow:hidden;white-space: nowrap; text-overflow: ellipsis;">{{ \App\Models\Queue::getSeverityName($log->severity) }}</td>
-                            <td style="width:20%;overflow:hidden;white-space: nowrap; text-overflow: ellipsis;">{{ mb_substr($log->file,0, 50) . (mb_strlen($log->file) > 50 ? '...' : '') }}</td>
-                            <td style="overflow:hidden;white-space: nowrap; text-overflow: ellipsis;">{{ $log->method }}</td>
-                            <td style="overflow:hidden;white-space: nowrap; text-overflow: ellipsis;width:30%;">{{ is_array($log->message) ?
+                            <td class="item" style="width:10%;">{{ \App\Models\Project::getProjectName($log->project_id) }}</td>
+                            <td class="item" style="width:10%;">{{ \App\Models\Queue::getSeverityName($log->severity) }}</td>
+                            <td class="item" style="width:20%;">{{ mb_substr($log->file,0, 50) . (mb_strlen($log->file) > 50 ? '...' : '') }}</td>
+                            <td class="item" style="">{{ $log->method }}</td>
+                            <td class="item" style="width:30%;">{{ is_array($log->message) ?
                                 str_replace("\n", "", mb_substr(print_r($log->message, true), 0, 150)) :
                                 str_replace("\n", "", trim(mb_substr($log->message, 0, 150))) }}</td>
                         </tr>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,6 +12,7 @@
 
     <!-- Styles -->
     <link href="/css/app.css" rel="stylesheet">
+    <link href="/css/custom.css" rel="stylesheet">
     <link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap.min.css">
 
     <!-- Scripts -->
