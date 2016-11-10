55d375d95703eb33f80d2117604fdbe377c5022e
Maris Jankovskis <maarisjankovskis@gmail.com>
2016-11-10 22:36:35 +0200

    asd

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,29 +7,29 @@
             <div class="panel panel-default">
                 <div class="panel-heading">Dashboard</div>
                 <table class="table table-condensed"
-                    style="overflow:hidden;white-space: nowrap; text-emphasis: circle;">
+                    style="width:100%;table-layout: fixed;">
                     <thead>
                         <tr>
-                            <th style="width:5%;overflow:hidden;white-space: nowrap; text-overflow: ellipsis;">ID</th>
-                            <th style="width:10%;overflow:hidden;white-space: nowrap; text-overflow: ellipsis;">Date</th>
-                            <th style="width:10%;overflow:hidden;white-space: nowrap; text-overflow: ellipsis;">Project</th>
-                            <th style="width:10%;overflow:hidden;white-space: nowrap; text-overflow: ellipsis;">Severity</th>
-                            <th style="width:20%;overflow:hidden;white-space: nowrap; text-overflow: ellipsis;">File</th>
-                            <th style="overflow:hidden;white-space: nowrap; text-overflow: ellipsis;">Method</th>
-                            <th style="overflow:hidden;white-space: nowrap; text-overflow: ellipsis;">Message</th>
+                            <th>ID</th>
+                            <th>Date</th>
+                            <th>Project</th>
+                            <th>Severity</th>
+                            <th>File</th>
+                            <th>Method</th>
+                            <th>Message</th>
                         </tr>
                     </thead>
                 @if ($logs)
                     @foreach ($logs as $log)
                         <tr class="{{ \App\Helper::getSeverityClass($log->severity) }}">
-                            <td><a href="{{ url('/log/' . $log->id) }}" title="{{ $log->id }}">Open</a></td>
-                            <td>{{ \App\Helper::dateToDiff($log->creation_date) }}<br/>
+                            <td style="width:5%;overflow:hidden;white-space: nowrap; text-overflow: ellipsis;"><a href="{{ url('/log/' . $log->id) }}" title="{{ $log->id }}">Open</a></td>
+                            <td style="width:10%;overflow:hidden;white-space: nowrap; text-overflow: ellipsis;">{{ \App\Helper::dateToDiff($log->creation_date) }}<br/>
                                 <span class="small">{{ $log->creation_date }}</span></td>
-                            <td>{{ \App\Models\Project::getProjectName($log->project_id) }}</td>
-                            <td>{{ \App\Models\Queue::getSeverityName($log->severity) }}</td>
-                            <td>{{ mb_substr($log->file,0, 50) . (mb_strlen($log->file) > 50 ? '...' : '') }}</td>
-                            <td>{{ $log->method }}</td>
-                            <td width="40%">{{ is_array($log->message) ?
+                            <td style="width:10%;overflow:hidden;white-space: nowrap; text-overflow: ellipsis;">{{ \App\Models\Project::getProjectName($log->project_id) }}</td>
+                            <td style="width:10%;overflow:hidden;white-space: nowrap; text-overflow: ellipsis;">{{ \App\Models\Queue::getSeverityName($log->severity) }}</td>
+                            <td style="width:20%;overflow:hidden;white-space: nowrap; text-overflow: ellipsis;">{{ mb_substr($log->file,0, 50) . (mb_strlen($log->file) > 50 ? '...' : '') }}</td>
+                            <td style="overflow:hidden;white-space: nowrap; text-overflow: ellipsis;">{{ $log->method }}</td>
+                            <td style="overflow:hidden;white-space: nowrap; text-overflow: ellipsis;" width="40%">{{ is_array($log->message) ?
                                 str_replace("\n", "", mb_substr(print_r($log->message, true), 0, 150)) :
                                 str_replace("\n", "", trim(mb_substr($log->message, 0, 150))) }}</td>
                         </tr>
